64b64b4626830fd3195aabb5db7f5b99abf9fd76
mva <milanovf7@mail.ru>
2026-05-13 14:52:38 +0300

    Update.

--- src/rpm_spec_editor/app.py
+++ src/rpm_spec_editor/app.py
@@ -1,10 +1,22 @@
 import sys
+from pathlib import Path
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
 
-from gui.main_window import MainWindow
+from rpm_spec_editor.gui.main_window import MainWindow
 
 def main():
     app = QApplication(sys.argv)
+
+    icon = QIcon.fromTheme("rpm-spec-editor")
+    if icon.isNull():
+        base_dir = Path(__file__).resolve().parent
+        local_icon = base_dir / "assets" / "SEicon.png"
+
+        if local_icon.exists():
+            icon = QIcon(str(local_icon))
+    app.setWindowIcon(icon)
+
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
